909791aa82354ff756d6e99c20848455a8b4402a
steffanhalv <steffanhalv@gmail.com>
2019-10-31 16:17:02 +0100

    Cleaned up

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
-
 include_once __DIR__ . 'lib/mail.php';
-
 class Gue {
   public function send_mail($from, $to, $subject, $message) {
 		return GueMail::send_mail($from, $to, $subject, $message);
